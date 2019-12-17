7d4067c0dee6fac5d9d7d10136ffe7a60ff3aece
taozhou1 <taozhou1@chinaums.com>
2019-12-17 18:07:21 +0800

    事务测试

--- src/main/java/henry/transaction/test/demo/controller/PeopleController.java
+++ src/main/java/henry/transaction/test/demo/controller/PeopleController.java
@@ -20,4 +20,31 @@ public class PeopleController {
         return "SUC";
     }
 
+    @GetMapping("/insert3")
+    @ResponseBody
+    public String insert3(){
+        peopleService.insert3();
+        return "SUC";
+    }
+
+    @GetMapping("/insert4")
+    @ResponseBody
+    public String insert4(){
+        peopleService.insert4();
+        return "SUC";
+    }
+
+    @GetMapping("/insert5")
+    @ResponseBody
+    public String insert5(){
+        peopleService.insert5();
+        return "SUC";
+    }
+
+    @GetMapping("/insert7")
+    @ResponseBody
+    public String insert7(){
+        peopleService.insert7();
+        return "SUC";
+    }
 }
--- src/main/java/henry/transaction/test/demo/service/PeopleService.java
+++ src/main/java/henry/transaction/test/demo/service/PeopleService.java
@@ -11,6 +11,7 @@ public class PeopleService {
     @Autowired
     PeopleRepository peopleRepository;
 
+    //事务方法调用事务方法，发生异常正常回滚
     @Transactional
     public void insert(){
         People people = new People();
@@ -23,7 +24,6 @@ public class PeopleService {
 
 		peopleRepository.save(people);
 		peopleRepository.save(people2);
-
         insert2();
     }
 
@@ -36,4 +36,66 @@ public class PeopleService {
         peopleRepository.save(people2);
         int i=1/0;
     }
+
+    //无事务注解，插入抛异常，插入成功
+    public void insert3(){
+        People people = new People();
+        people.setId("222");
+        people.setName2("Henry1");
+
+        peopleRepository.save(people);
+        int i=1/0;
+    }
+
+    //有事务注解，插入抛异常，回滚
+    @Transactional
+    public void insert4(){
+        People people = new People();
+        people.setId("333");
+        people.setName2("Henry1");
+
+        peopleRepository.save(people);
+        int i=1/0;
+    }
+
+    //有事务方法，调用非事务方法，非事务方法异常 回滚
+    @Transactional
+    public void insert5(){
+        People people = new People();
+        people.setId("5555");
+        people.setName2("Henry1");
+
+        peopleRepository.save(people);
+        insert6();
+    }
+
+    public void insert6(){
+        People people = new People();
+        people.setId("6666");
+        people.setName2("Henry1");
+
+        peopleRepository.save(people);
+        int i=1/0;
+    }
+
+    //非事务方法，调用事务方法，事务方法异常 ,不回滚
+    public void insert7(){
+        People people = new People();
+        people.setId("77777");
+        people.setName2("Henry1");
+
+        peopleRepository.save(people);
+        insert8();
+    }
+
+    @Transactional
+    public void insert8(){
+        People people = new People();
+        people.setId("8888");
+        people.setName2("Henry1");
+
+        peopleRepository.save(people);
+        int i=1/0;
+    }
+
 }
